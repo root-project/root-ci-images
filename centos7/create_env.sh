7d473377010a57c27c2e256cafc5124d0384bed3
olemorud <ole.kristian.morud@cern.ch>
2022-09-21 13:55:40 +0200

    update centos7

--- centos7/create_env.sh
+++ centos7/create_env.sh
@@ -1,44 +1,34 @@
 
-# Run this script inside centos7 to print
-# the environment needed to compile ROOT to stdout
+source /cvmfs/sft.cern.ch/lcg/releases/binutils/2.30-e5b21/x86_64-centos7/setup.sh
 
+BASE=/cvmfs/sft.cern.ch/lcg/releases/gcc/8.3.0-cebb0/x86_64-centos7
 
-if [ -e "${BINUTILS_BASE}/lib64" ]; then
-     LD_LIBRARY_PATH="$BINUTILS_BASE/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export PATH=$BASE/bin:$PATH
+export MANPATH=$BASE/share/man:$MANPATH
+
+if [ -e "${BASE}/lib64" ]; then
+    export LD_LIBRARY_PATH="$BASE/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 fi
-if [ -e "${BINUTILS_BASE}/lib" ]; then
+if [ -e "${BASE}/lib" ]; then
     # Add lib if exists
-     LD_LIBRARY_PATH="$BINUTILS_BASE/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+    export LD_LIBRARY_PATH="$BASE/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 fi
 
-
-BINUTILS_BASE=/cvmfs/sft.cern.ch/lcg/releases/binutils/2.30-e5b21/x86_64-centos7
-GCC_BASE=/cvmfs/sft.cern.ch/lcg/releases/gcc/8.3.0-cebb0/x86_64-centos7
-
-
-if [ -e "${GCC_BASE}/lib64" ]; then
-     LD_LIBRARY_PATH="$GCC_BASE/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-fi
-if [ -e "${GCC_BASE}/lib" ]; then
-    # Add lib if exists
-     LD_LIBRARY_PATH="$GCC_BASE/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-fi
+# Export package specific environmental variables
 
 gcc_home=/cvmfs/sft.cern.ch/lcg/releases/gcc/8.3.0-cebb0/x86_64-centos7 
 
-FC=`which gfortran`
-CC=`which gcc`
-CXX=`which g++`
-COMPILER_PATH=${gcc_home}
+export FC=`which gfortran`
+export CC=`which gcc`
+export CXX=`which g++`
 
-MANPATH=$BINUTILS_BASE/share/man:$GCC_BASE/share/man
-PATH=$BINUTILS_BASE/bin:$GCC_BASE/bin
+export COMPILER_PATH=${gcc_home}
 
-echo "PATH=\$PATH:$PATH"
-echo "MANPATH=$MANPATH"
-echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
-echo "FC=$FC"
-echo "CC=$CC"
-echo "CXX=$CXX"
-echo "COMPILER_PATH=$COMPILER_PATH"
 
+echo PATH=$PATH
+echo MANPATH=$MANPATH
+echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+echo FC=$FC
+echo CC=$CC
+echo CXX=$CXX
+echo COMPILER_PATH=$COMPILER_PATH
